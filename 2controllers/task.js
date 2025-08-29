d5442c02f6e8ef4a051905d764507ad0961e6eee
mantasha-shaikh <mantasha2513604@gmail.com>
2025-08-29 23:33:09 +0530

    hey this my fial commit 2 nd projects

--- 2controllers/task.js
+++ 2controllers/task.js
@@ -19,6 +19,7 @@ const getAllTasks = asyncWrapper(async (req, res) => {
 const createTask = asyncWrapper(async (req, res) => {
   
         const task = await Task.create(req.body);
+        
         res.status(200).json({ task }); // check in Postman (Body -> raw -> JSON)
         console.log(task);
    
